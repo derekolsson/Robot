9d3bc420034e190d150dd75cb085ba049e32fcb5
derekolsson <derekolsson@gmail.com>
2015-10-27 19:20:59 -0600

    Finally Compiles
    Had to change count to cont
    
    Compiles successfully in SIMPLEIDE

--- mazeRunner.c
+++ mazeRunner.c
@@ -8,14 +8,14 @@
 int advance;                               
 
 /*
-	map[count][direction]
+	map[cont][direction]
 	directions:	0 = forward, the direction AB entered from
 				1 = left
 				2 = right
 */				
 int map[500][2];							  
 //which "section" we are in
-int count;
+int cont;
 
 //will check for openings (0 open, 1 closed)
 int checkWall();
@@ -34,37 +34,37 @@ void goBack();
 
 int main()                                  // main 
 {
-	count = 0;
+	cont = 0;
 	advance = 1;
 
-	  for(;;){									//endless loop
+	  while(1){									//endless loop
 		while (advance == 1){					//if advancing
-			checkSpace();						//first check the space
+			checkSpace(cont);						//first check the space
 			
 			//priority to go: Right, Straight, Left
 			//after, increment counter
 			
-			if (map[count][2]== 0){				
+			if (map[cont][2]== 0){				
 				goForward();
-				count = count + 1;
+				cont = cont + 1;
 			}
 			
-			else if (map[count][0] == 0){
+			else if (map[cont][0] == 0){
 				turnLeft();
 				goForward();
-				count = count + 1;
+				cont = cont + 1;
 			}
 			
-			else if (map[count][1] == 0){
+			else if (map[cont][1] == 0){
 				turnAround();
 				goForward();
-				count = count + 1;
+				cont = cont + 1;
 			}
 			
 			//if nothing, face "forward", return to last square, begin backwards routine
 			else{
 				turnLeft();
-				count = count - 1;
+				cont = cont - 1;
 				goBack();
 				advance = 0;
 			}
@@ -73,30 +73,30 @@ int main()                                  // main
 		//backwards routine 
 		while (advance == 0){
 			
-			//decrement count to see prior space
-			count = count - 1;
+			//decrement cont to see prior space
+			cont = cont - 1;
 			//check in order: Right, Forward, Left from last space
 			//turn so back facing opening, then go backwards
 			//"close" direction you came from
 			
 			//Right
-			if (map[count][2] == 0){
+			if (map[cont][2] == 0){
 				turnLeft();			//face opening
 				goBack();			//go backwards
 				turnLeft();			//face "forward"
-				map[count][2] == 1;	//close right
+				map[cont][2] == 1;	//close right
 			}
 			//Forward
-			else if (map[count][0] == 0){
+			else if (map[cont][0] == 0){
 				goBack();
-				map[count][0] == 1;
+				map[cont][0] == 1;
 			}
 			//Left
-			else if (map[count][1] == 0){
+			else if (map[cont][1] == 0){
 				turnRight();
 				goBack();
 				turnRight;
-				map[count][1] == 1;
+				map[cont][1] == 1;
 			}
 			
 			//if no openings left, it's basically stuck, so break routine
@@ -106,24 +106,24 @@ int main()                                  // main
 			
 			//restart advance
 			//tries right, then straight, then left
-			if (map[count][2] == 0){
+			if (map[cont][2] == 0){
 				turnRight();				//face right
 				goForward();				//go forward
-				count = count + 1;			//increment count
+				cont = cont + 1;			//increment cont
 				advance = 1;				//restart advance loop
 			}
 			
 			//straight
-			else if (map[count][0] == 0){
+			else if (map[cont][0] == 0){
 				goForward();
-				count = count + 1;
+				cont = cont + 1;
 				advance = 1;
 			}
 			//left
-			else if (map[count][1] == 0){
+			else if (map[cont][1] == 0){
 				turnRight();
 				goForward();
-				count = count + 1;
+				cont = cont + 1;
 				advance = 1;
 			}
 			
@@ -132,28 +132,29 @@ int main()                                  // main
 	}
 	return 0;
 }
-	int			
-	checkWall(){		//If there's a wall within 5cm, return 1, else 0
 
-		if (ping_cm(8) < 5){
-			return 1;
-		}
-		else{
-			return 0;
-		}
+int			
+checkWall(){		//If there's a wall within 5cm, return 1, else 0
+
+	if (ping_cm(8) < 5){
+		return 1;
 	}
+	else{
+		return 0;
+	}
+}
 
 void
-checkSpace(){		//Check each space for straight, left then right
-	map[count][0] = checkWall();
+checkSpace(int i){		//Check each space for straight, left then right
+	map[i][0] = checkWall();
 	
 	turnLeft();
 	
-	map[count][1] = checkWall();
+	map[i][1] = checkWall();
 	
 	turnAround();
 	
-	map[count][2]= checkWall();
+	map[i][2]= checkWall();
 	
 	return;
 	}
